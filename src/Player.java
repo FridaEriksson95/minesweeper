afe0df3eec00017096cfabe6d6ba04ddbde1ec12
robinjakobsson <3ggapp24.jakoro@folkuniversitetet.nu>
2024-10-24 12:13:25 +0200

    Created methods and constructor for player class

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,42 @@
 public class Player {
+    int winCount;
+    int loseCount;
+    int points;
+    String name;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public int getWinCount() {
+        return winCount;
+    }
+
+    public void setWinCount(int winCount) {
+        this.winCount = winCount;
+    }
+
+    public int getLoseCount() {
+        return loseCount;
+    }
+
+    public void setLoseCount(int loseCount) {
+        this.loseCount = loseCount;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
